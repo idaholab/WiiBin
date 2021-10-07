57290f810f91f55d00f5ab862680c8afc019637f
Bryan <33437140+bryan-inl@users.noreply.github.com>
2021-10-06 22:08:06 -0600

    Update WiiBin.py
    Minor entropy percentage color bug fixed

--- WiiBin.py
+++ WiiBin.py
@@ -233,15 +233,14 @@ def fileSelectButton():
    
    entropyNumber = float(entropyPercent.split('%')[0])
    
-   if entropyNumber >= 0:
-      percentLabel.config(fg="green")
-   if entropyNumber >= 33:
-      percentLabel.config(fg="orange")
-      messagebox.showwarning("Entropy Warning", "Warning: Selected Binary is " + str(entropyPercent) + " Compressed or Encrypted.  WiiBin Results Might Not Be Reliable.")
    if entropyNumber >= 66:
       percentLabel.config(fg="red")
       messagebox.showwarning("Entropy Warning", "Warning: Selected Binary is " + str(entropyPercent) + " Compressed or Encrypted.  WiiBin Results Should Not Be Trusted.")
-   
+   elif entropyNumber >= 33:
+      percentLabel.config(fg="orange")
+      messagebox.showwarning("Entropy Warning", "Warning: Selected Binary is " + str(entropyPercent) + " Compressed or Encrypted.  WiiBin Results Might Not Be Reliable.")
+   elif entropyNumber >= 0:
+      percentLabel.config(fg="green")
 
 def entropyLabelClicked(event):
    resultsBox.delete(1.0, END)
@@ -386,7 +385,7 @@ scrollb = Scrollbar(root, command=resultsBox.yview)
 scrollb.place(x=630, y=350)
 resultsBox['yscrollcommand'] = scrollb.set
 
-resultsBox.insert(END,'Welcome to WiiBin 1.7\n----------------------\n\nNotes:\n\nThe smaller the Slide (%) and Chunk Size, the more accurate the detection and longer the runtime.\n\nToo small a Chuck Size will cause make ML difficult and less accurate.\n\nThe minimum detectable code segment size is limited to half of the selected Chunk Size.\n\nTo detect smaller code segments the Chunk Size most be reduced.')
+resultsBox.insert(END,'Welcome to WiiBin 1.7.1\n----------------------\n\nNotes:\n\nThe smaller the Slide (%) and Chunk Size, the more accurate the detection and longer the runtime.\n\nToo small a Chuck Size will cause make ML difficult and less accurate.\n\nThe minimum detectable code segment size is limited to half of the selected Chunk Size.\n\nTo detect smaller code segments the Chunk Size most be reduced.')
 
 clearDirectories()
 
